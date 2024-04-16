70e695632f609618e8ef94ae2b0930273a897960
Katkar213 <blackship213@gmail.com>
2024-04-16 14:10:29 +0530

    all

--- js/script.js
+++ js/script.js
@@ -16,3 +16,15 @@ const buttons=document.querySelectorAll("button");
 
 
 // slider...
+const images = document.getElementsByClassName("crousal-image");
+const imagelink = ["./images/crousal-first-image.JPG","./images/crousal-second-image.JPG","./images/hero-session-image.jpg", "./images/hero-session-image2.jpg", "./images/hero-session-image3.jpg"];
+let count = 0;
+
+setInterval(() => {
+    // Set the source of the current image
+    images[0].src = imagelink[count];
+    
+    // Increment the count and reset if it exceeds the length of imagelink
+    count = (count + 1) % imagelink.length;
+}, 3000);
+
